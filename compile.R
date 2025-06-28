4e03a7849b2baf2f48fcab2860f29c0c3f108318
zyang91 <zhanchao@upenn.edu>
2025-06-27 23:55:16 -0400

    some left

--- compile.R
+++ compile.R
@@ -18,7 +18,7 @@ line2<- hog%>%
 
 
 ######## 
-# general information availability
+# metro attitute
 count_pct <- function(data, var) {
   data %>%
     count({{ var }}) %>%
@@ -66,6 +66,9 @@ p64_line2<- count_pct(line2, P64)
 p68_line1<- count_pct(line1, P68)
 p68_line2<- count_pct(line2, P68)
 
+#P78: prefer metro over other transportation modes
+p78_line1 <- count_pct(line1, P78)
+p78_line2 <- count_pct(line2, P78)
 
 ########
 # evaluate metro impacts
@@ -118,3 +121,170 @@ p100_line2 <- three_opt(line2, P100)
 p101_line1 <- three_opt(line1, P101)
 p101_line2 <- three_opt(line2, P101)
 
+########
+# Information availability
+
+five_opt <- function(data, var) {
+  data %>%
+    count({{ var }}) %>%
+    mutate(
+      Percentage = n / sum(n) * 100
+    ) %>%
+    mutate(
+      {{ var }} := case_when(
+        {{ var }} == 1 ~ "No Information at all",
+        {{ var }} == 2 ~ "Little Information",
+        {{ var }} == 3 ~ "Some Information",
+        {{ var }} == 4 ~ "A lot of Information",
+        {{ var }} == 5 ~ "All Information",
+        TRUE         ~ as.character({{ var }})
+      )
+    )
+}
+
+# P70: type of metro information
+p70_line1 <- five_opt(line1, P70)
+p70_line2 <- five_opt(line2, P70)
+
+# P71: metro route
+p71_line1 <- five_opt(line1, P71)
+p71_line2 <- five_opt(line2, P71)
+
+# P72: station location
+p72_line1 <- five_opt(line1, P72)
+p72_line2 <- five_opt(line2, P72)
+
+# P73: construction timeline
+p73_line1 <- five_opt(line1, P73)
+p73_line2 <- five_opt(line2, P73)
+
+
+##### other
+# P83: living time
+p83_line1 <- line1 %>%
+  filter(!is.na(P83)) %>%
+  group_by(P83) %>%
+  summarise(Count = n()) %>%
+  mutate(
+    Percentage_length = (Count / sum(Count)) * 100,
+    Age_group = case_when(
+      P83 == 1 ~ "Less than or equal to 1 year",
+      P83 == 2 ~ "More than 1 and up to 5 years",
+      P83 == 3 ~ "More than 5 and up to 10 years",
+      P83 == 4 ~ "More than 10 and up to 15 years",
+      P83 == 5 ~ "More than 15 and up to 20 years",
+      P83 == 6 ~ "More than 20 years"
+    )
+  ) 
+
+P83_line2 <- line2 %>%
+  filter(!is.na(P83)) %>%
+  group_by(P83) %>%
+  summarise(Count = n()) %>%
+  mutate(
+    Percentage_length = (Count / sum(Count)) * 100,
+    Age_group = case_when(
+      P83 == 1 ~ "Less than or equal to 1 year",
+      P83 == 2 ~ "More than 1 and up to 5 years",
+      P83 == 3 ~ "More than 5 and up to 10 years",
+      P83 == 4 ~ "More than 10 and up to 15 years",
+      P83 == 5 ~ "More than 15 and up to 20 years",
+      P83 == 6 ~ "More than 20 years"
+    )
+  )
+
+
+# P82: rent and own
+p82_line1<- line1 %>%
+  count(P82) %>%
+  mutate(
+    P82 = case_when(
+      P82 == 1 ~ "Own",
+      P82 == 2 ~ "Rent",
+      TRUE     ~ as.character(P82)
+    ),
+    Percentage = (n / sum(n)) * 100
+  )
+
+p82_line2<- line2 %>%
+  count(P82) %>%
+  mutate(
+    P82 = case_when(
+      P82 == 1 ~ "Own",
+      P82 == 2 ~ "Rent",
+      TRUE     ~ as.character(P82)
+    ),
+    Percentage = (n / sum(n)) * 100
+  )
+
+# P86: renting cost
+
+p86_line1 <- line1%>%
+  filter(!is.na(P86) & P86 != 99) %>%
+  group_by(P86) %>%
+  summarise(Count = n()) %>%
+  mutate(
+    Percentage_rent = (Count / sum(Count)) * 100,
+    Rent_range = case_when(
+      P86 == 1  ~ "Less than or equal to $500,000",
+      P86 == 2  ~ "More than $500,000 and up to $1,000,000",
+      P86 == 3  ~ "More than $1,000,000 and up to $1,500,000",
+      P86 == 4  ~ "More than $1,500,000 and up to $2,000,000",
+      P86 == 5  ~ "More than $2,000,000 and up to $3,000,000",
+      P86 == 6  ~ "More than $3,000,000 and up to $4,000,000",
+      P86 == 7  ~ "More than $4,000,000 and up to $5,000,000",
+      P86 == 8  ~ "More than $5,000,000"
+    )
+  )
+
+p86_line2 <- line2 %>%
+  filter(!is.na(P86) & P86 != 99) %>%
+  group_by(P86) %>%
+  summarise(Count = n()) %>%
+  mutate(
+    Percentage_rent = (Count / sum(Count)) * 100,
+    Rent_range = case_when(
+      P86 == 1  ~ "Less than or equal to $500,000",
+      P86 == 2  ~ "More than $500,000 and up to $1,000,000",
+      P86 == 3  ~ "More than $1,000,000 and up to $1,500,000",
+      P86 == 4  ~ "More than $1,500,000 and up to $2,000,000",
+      P86 == 5  ~ "More than $2,000,000 and up to $3,000,000",
+      P86 == 6  ~ "More than $3,000,000 and up to $4,000,000",
+      P86 == 7  ~ "More than $4,000,000 and up to $5,000,000",
+      P86 == 8  ~ "More than $5,000,000"
+    )
+  )
+
+# P65_1: most important factor in travel
+
+p65_line1 <- line1 %>%
+  count(P65_1) %>%
+  mutate(
+    P65_1 = case_when(
+      P65_1 == 1 ~ "Time of trip",
+      P65_1 == 2 ~ "Closeness of station",
+      P65_1 == 3 ~ "Comfort during trip",
+      P65_1 == 4 ~ "Security on the system",
+      P65_1 == 5 ~ "Cost of travel",
+      P65_1 == 6 ~ "Environmental impact of transport mode",
+      P65_1 == 7 ~ "Punctuality of buses",
+      TRUE      ~ NA_character_   # catch any unexpected codes
+    ),
+    Percentage = n / sum(n) * 100
+  )
+
+p65_line2 <- line2 %>%
+  count(P65_1) %>%
+  mutate(
+    P65_1 = case_when(
+      P65_1 == 1 ~ "Time of trip",
+      P65_1 == 2 ~ "Closeness of station",
+      P65_1 == 3 ~ "Comfort during trip",
+      P65_1 == 4 ~ "Security on the system",
+      P65_1 == 5 ~ "Cost of travel",
+      P65_1 == 6 ~ "Environmental impact of transport mode",
+      P65_1 == 7 ~ "Punctuality of buses",
+      TRUE      ~ NA_character_   # catch any unexpected codes
+    ),
+    Percentage = n / sum(n) * 100
+  )
